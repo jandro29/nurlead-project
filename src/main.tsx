099eff5c7916936da1228f3abcd94c980e404153
Sebastian Alfaro <sebastian.alfaromiranda@gmail.com>
2025-11-15 21:37:18 -0500

    modularize CSS animations and keyframes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 //main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import '@/styles/index.css'
+import '@/styles/animations.css'
+import '@/styles/keyframes.css'
 import App from './App.tsx'
 
 
